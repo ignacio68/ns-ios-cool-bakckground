f84e346a0257014e99558fcce6e1b1e39318ef5a
Ignacio López-Amor <ilopez-amor@ya.com>
2024-01-16 12:37:52 +0100

    feat: add Swift symbol

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
-  import { ref, computed, onMounted, onUnmounted, $navigateTo } from "nativescript-vue";
+  import { ref } from "nativescript-vue";
+  const symbol = ref("multiply.circle.fill");
+  const changeIcon = () => {
+    symbol.value = symbol.value === "menucard" ? "multiply.circle.fill" : "menucard";
+  };
 </script>
 
 <template>
@@ -8,11 +12,14 @@
       <ActionBar>
         <Label text="Home" class="font-bold text-lg" />
       </ActionBar>
-
-      <GridLayout class="bg-black">
+      <stackLayout>
+        <symbolView class="h-[50] w-[50] mt-28" :symbol="symbol" />
+        <button text="Change icon" class="text-center mt-12 text-2xl" @tap="changeIcon" />
+      </stackLayout>
+      <!-- <GridLayout class="bg-black">
         <SwiftUI swiftId="coolBackground" class="w-full h-full"></SwiftUI>
-        <!-- <Layout class="bg-transparent text-center text-3xl font-semibold from-neutral-300" text="Esto es una prueba" /> -->
-      </GridLayout>
+        <Layout class="bg-transparent text-center text-3xl font-semibold from-neutral-300" text="Esto es una prueba" />
+      </GridLayout> -->
     </Page>
   </Frame>
 </template>
