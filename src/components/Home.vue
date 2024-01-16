3d1d6854bdc1fc92749020b12fbe680ade550747
Ignacio López-Amor <ilopez-amor@ya.com>
2024-01-16 12:30:31 +0100

    feat: add metal Swift element

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,32 +1,5 @@
 <script lang="ts" setup>
-import {
-  ref,
-  computed,
-  onMounted,
-  onUnmounted,
-  $navigateTo,
-} from 'nativescript-vue';
-import Details from './Details.vue';
-
-const counter = ref(0);
-const message = computed(() => {
-  return `Blank {N}-Vue app: ${counter.value}`;
-});
-
-function logMessage() {
-  console.log('You have tapped the message!');
-}
-
-let interval: any;
-onMounted(() => {
-  console.log('mounted');
-  interval = setInterval(() => counter.value++, 100);
-});
-
-onUnmounted(() => {
-  console.log('unmounted');
-  clearInterval(interval);
-});
+  import { ref, computed, onMounted, onUnmounted, $navigateTo } from "nativescript-vue";
 </script>
 
 <template>
@@ -36,29 +9,16 @@ onUnmounted(() => {
         <Label text="Home" class="font-bold text-lg" />
       </ActionBar>
 
-      <GridLayout rows="*, auto, auto, *" class="px-4">
-        <Label
-          row="1"
-          class="text-xl align-middle text-center text-gray-500"
-          :text="message"
-          @tap="logMessage"
-        />
-
-        <Button
-          row="2"
-          @tap="$navigateTo(Details)"
-          class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
-          horizontalAlignment="center"
-        >
-          View Details
-        </Button>
+      <GridLayout class="bg-black">
+        <SwiftUI swiftId="coolBackground" class="w-full h-full"></SwiftUI>
+        <!-- <Layout class="bg-transparent text-center text-3xl font-semibold from-neutral-300" text="Esto es una prueba" /> -->
       </GridLayout>
     </Page>
   </Frame>
 </template>
 
 <style>
-/* .info {
+  /* .info {
     font-size: 20;
   } */
 </style>
